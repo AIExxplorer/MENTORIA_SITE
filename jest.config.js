4b5d6e9f4342727416da9b8bcb9b7a5755224bd9
Wagner Rafael | AIExxplorer <202305188631@alunos.estacio.br>
2025-07-29 20:52:32 -0300

    feat: add Jest configuration for unit testing

---
+++ jest.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/js/setupTests.js'],
+  moduleNameMapping: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/__mocks__/fileMock.js',
+  },
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx}',
+    '!src/**/*.test.{js,jsx}',
+    '!src/js/setupTests.js',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.{js,jsx}',
+    '<rootDir>/src/**/*.{test,spec}.{js,jsx}',
+  ],
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest',
+  },
+  verbose: true,
+}; 
