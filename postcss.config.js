7489616904ff9512019a9d97b877a3e4cd3b0e18
Wagner Rafael | AIExxplorer <202305188631@alunos.estacio.br>
2025-07-29 20:53:07 -0300

    feat: add PostCSS configuration for CSS processing

---
+++ postcss.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  plugins: [
+    require('autoprefixer'),
+    require('cssnano')({
+      preset: [
+        'default',
+        {
+          discardComments: {
+            removeAll: true,
+          },
+          normalizeWhitespace: true,
+          colormin: true,
+          minifyFontValues: true,
+          minifyGradients: true,
+          minifyParams: true,
+          minifySelectors: true,
+        },
+      ],
+    }),
+  ],
+}; 
